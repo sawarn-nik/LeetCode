17c91f8a87b8c41955a5e9ff9ca6fc7f62eb1dd1
Nikhil Mishra <142802377+sawarn-nik@users.noreply.github.com>
2026-07-07 14:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (27.38%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     long long sumAndMultiply(int n) {
         long long res = 0;
-        long long x = 0;
+        string x = "";
         long long sum = 0;
         string tem = to_string(n);
         for(int i = 0; i < tem.length(); i++){
             if(tem[i] == '0') continue;
-            x += tem[i] - '0';
-            x*=10;
+            x += tem[i];
             sum += tem[i]-'0';
         }
-        x/=10;
-        res = x*sum;
+        if (x.empty())
+            return 0;
+        res = 1LL*stoi(x)*sum;
         return res;
     }
 };
