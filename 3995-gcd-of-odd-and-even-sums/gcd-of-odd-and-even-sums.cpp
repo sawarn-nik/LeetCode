98058a472a8ce10947e38c794d5dcfcd58aefbec
Nikhil Mishra <142802377+sawarn-nik@users.noreply.github.com>
2026-07-15 10:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (46.67%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -2,8 +2,9 @@ class Solution {
 
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumo = (n*n);
-        int sume = (n*(n+1));
-        return __gcd(sumo,sume);
+        // int sumo = (n*n);
+        // int sume = (n*(n+1));
+        // return __gcd(sumo,sume);
+        return n;
     }
 };
