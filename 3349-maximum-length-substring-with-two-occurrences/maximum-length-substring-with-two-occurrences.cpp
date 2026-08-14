5803288a86aad3edf8e4e8a4c95119f98d33d5ff
Nikhil Mishra <142802377+sawarn-nik@users.noreply.github.com>
2026-08-14 13:40:19 +0530

    Time: 3 ms (42.93%) | Memory: 9.7 MB (44.69%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -5,21 +5,6 @@ public:
         int l = 0,i = 0, j = 0,n = s.size();
 
         while(j < n){
-            // cout<<i<<" "<<j<<endl;
-            if(mp.find(s[j]) == mp.end()){
-                mp[s[j]]++;
-                l = max(l,j-i+1);
-                j++;
-                continue;
-            }
-
-            if(mp[s[j]] < 2){
-                mp[s[j]]++;
-                l = max(l,j-i+1);
-                j++;
-            }
-
-            else{
                 while(i<j && mp[s[j]]>=2){
                     mp[s[i]]--;
                     i++;
@@ -28,7 +13,7 @@ public:
                 l = max(l,j-i+1);
                 j++;
             }
-        }
+        
 
         return l;
     }
