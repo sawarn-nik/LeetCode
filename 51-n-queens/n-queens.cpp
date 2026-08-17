f87e7312d6d0a6548108b5fd1ad7ae1c220d60b5
Nikhil Mishra <142802377+sawarn-nik@users.noreply.github.com>
2026-08-17 13:50:58 +0530

    Time: 11 ms (11.84%) | Memory: 13.1 MB (10.14%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,96 @@
+class Solution {
+public:
+    void solve(int t,int i, vector<string> &res,vector<vector<string>> &ress,int n){
+        // All queens placed
+        if(t == n) {
+            vector<string> temp = res;
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(temp[i][j] == 'X') {
+                        temp[i][j] = '.';
+                    }
+                }
+            }
+            ress.push_back(temp);
+            return;
+        }
+
+        if(i == n){
+            return;
+        }
+        for(int j = 0; j < n; j++) {
+            if(res[i][j] == '.')
+            {
+                vector<pair<int,int>> changed;
+                res[i][j] = 'Q';
+                for(int r = 0; r < n; r++) {
+                    if(r != i && res[r][j] == '.') {
+                        res[r][j] = 'X';
+                        changed.push_back({r,j});
+                    }
+                }
+                int r = i - 1;
+                int c = j - 1;
+                while(r >= 0 && c >= 0) {
+                    if(res[r][c] == '.') {
+                        res[r][c] = 'X';
+                        changed.push_back({r,c});
+                    }
+                    r--;
+                    c--;
+                }
+                r = i + 1;
+                c = j + 1;
+                while(r < n && c < n) {
+                    if(res[r][c] == '.') {
+                        res[r][c] = 'X';
+                        changed.push_back({r,c});
+                    }
+                    r++;
+                    c++;
+                }
+                r = i - 1;
+                c = j + 1;
+                while(r >= 0 && c < n) {
+                    if(res[r][c] == '.') {
+                        res[r][c] = 'X';
+                        changed.push_back({r,c});
+                    }
+                    r--;
+                    c++;
+                }
+                r = i + 1;
+                c = j - 1;
+                while(r < n && c >= 0) {
+                    if(res[r][c] == '.') {
+                        res[r][c] = 'X';
+                        changed.push_back({r,c});
+                    }
+                    r++;
+                    c--;
+                }
+                solve(t + 1, i + 1, res, ress, n);
+
+                res[i][j] = '.';
+
+                for(auto p : changed) {
+                    res[p.first][p.second] = '.';
+                }
+            }
+        }        
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ress;
+        vector<string> res;
+        for(int i = 0; i < n; i++){
+            string s = "";
+            for(int j = 0; j < n; j++){
+                s+=".";
+            }
+            res.push_back(s);
+        }
+        solve(0,0,res,ress,n);
+
+        return ress;
+    }
+};
