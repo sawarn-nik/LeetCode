dd34030b4f2d5399b74869d5d49f80e79d15c8fd
Nikhil Mishra <142802377+sawarn-nik@users.noreply.github.com>
2026-08-20 22:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.63%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1,arr2;
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+        int j = 0,k = 0;
+        for(int i = 2; i < nums.size(); i++){
+            if(arr1[j] < arr2[k]){
+                arr2.push_back(nums[i]);
+                k++;
+                continue;
+            }
+            if(arr1[j] > arr2[k]){
+                arr1.push_back(nums[i]);
+                j++;
+            }
+        }
+        arr1.insert(arr1.end(), arr2.begin(),arr2.end());
+        return arr1;
+    }
+};
