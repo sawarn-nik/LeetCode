3f4581e7b1b3804d04effdf80427c976f83616bf
Nikhil Mishra <142802377+sawarn-nik@users.noreply.github.com>
2026-08-24 01:48:56 +0530

    Time: 7 ms (21.62%) | Memory: 14 MB (33.02%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?')
+                q1++;
+            else
+                sum1 += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?')
+                q2++;
+            else
+                sum2 += num[i] - '0';
+        }
+
+        // Odd number of ? -> Alice wins
+        if ((q1 + q2) % 2 == 1)
+            return true;
+
+        // Bob wins only when exact balance is possible
+        return sum1 - sum2 != 9 * (q2 - q1) / 2;
+    }
+};
